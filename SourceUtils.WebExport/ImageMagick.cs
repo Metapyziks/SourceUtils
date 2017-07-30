c153e2c8b4bac5560cba81f97e76c6bdfdeff473
Ziks <metapyziks@gmail.com>
2017-07-30 22:12:26 +0100

    Working on a command line implementation of Magick.NET

--- SourceUtils.WebExport/ImageMagick.cs
+++ SourceUtils.WebExport/ImageMagick.cs
@@ -1,6 +1,9 @@
 #if LINUX
 using System;
 using System.IO;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
 
 namespace ImageMagick
 {
@@ -59,7 +62,7 @@ namespace ImageMagick
         private readonly byte _b;
         private readonly byte _a;
         
-        public Pixel(byte r, byte g, byte b)
+        public Pixel( byte r, byte g, byte b )
         {
             Channels = 3;
             _r = r;
@@ -68,7 +71,7 @@ namespace ImageMagick
             _a = 0;
         }
         
-        public Pixel(byte r, byte g, byte b, byte a)
+        public Pixel( byte r, byte g, byte b, byte a )
         {
             Channels = 4;
             _r = r;
@@ -81,7 +84,7 @@ namespace ImageMagick
         {
             get
             {
-                switch (channel)
+                switch ( channel )
                 {
                     case 0: return _r;
                     case 1: return _g;
@@ -93,59 +96,254 @@ namespace ImageMagick
         }
     }
     
-    public class ImagePixels
-    {
-        public Pixel this[int x, int y]
-        {
-            get { throw new NotImplementedException(); }
-        }
-    }
-    
     public class MagickImage : IDisposable
     {
-        public int Width { get; private set; }
-        public int Height { get; private set; }
-        
-        public int ChannelCount { get; private set; }
-        
-        public MagickImage( string filePath, MagickReadSettings readSettings )
+        [ThreadStatic]
+        private static StringBuilder _sArgBuilder;
+
+        private static void Cli( Stream input, MagickReadSettings inputReadSettings,
+            Stream output, MagickReadSettings outputReadSettings, params string[] args )
         {
-            throw new NotImplementedException();
+            if ( _sArgBuilder == null ) _sArgBuilder = new StringBuilder();
+            else _sArgBuilder.Remove( 0, _sArgBuilder.Length );
+
+            if ( input != null )
+            {
+                switch ( inputReadSettings.Format )
+                {
+                    case MagickFormat.Default:
+                        _sArgBuilder.Append( $" -size {inputReadSettings.Width}x{inputReadSettings.Height}" );
+                        _sArgBuilder.Append( $" -depth {8}" );
+                        _sArgBuilder.Append( $" {inputReadSettings.PixelStorage.ChannelFormat.ToString().ToLower()}:-");
+                        break;
+                    case MagickFormat.Png:
+                        _sArgBuilder.Append( $" png:-" );
+                        break;
+                    case MagickFormat.Dds:
+                        _sArgBuilder.Append( $" dds:-" );
+                        break;
+                    default: throw new NotImplementedException();
+                }
+            }
+
+            foreach ( var arg in args )
+            {
+                _sArgBuilder.Append( $" \"{arg}\"" );
+            }
+
+            if ( output != null )
+            {
+                switch ( outputReadSettings.Format )
+                {
+                    case MagickFormat.Default:
+                        _sArgBuilder.Append( $" {outputReadSettings.PixelStorage.ChannelFormat.ToString().ToLower()}:-");
+                        break;
+                    case MagickFormat.Png:
+                        _sArgBuilder.Append( $" png:-" );
+                        break;
+                    case MagickFormat.Dds:
+                        _sArgBuilder.Append( $" dds:-" );
+                        break;
+                    default: throw new NotImplementedException();
+                }
+            }
+
+            var process = new Process
+            {
+                StartInfo =
+                {
+                    FileName = "convert",
+                    Arguments = _sArgBuilder.ToString(),
+                    CreateNoWindow = true,
+                    RedirectStandardInput = true,
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false
+                }
+            };
+
+            process.Start();
+
+            if ( input != null )
+            {
+                input.CopyTo( process.StandardInput.BaseStream );
+            }
+
+            process.WaitForExit();
+            
+            if ( output != null )
+            {
+                process.StandardOutput.BaseStream.CopyTo( output );
+            }
+        }
+
+        public class Pixels
+        {
+            public Pixel this[int x, int y]
+            {
+                get
+                {
+                    var baseIndex = _rowStride * y + _channels * x;
+                    return new Pixel(
+                        _pixelData[baseIndex + _rloc],
+                        _pixelData[baseIndex + _gloc],
+                        _pixelData[baseIndex + _bloc],
+                        _pixelData[baseIndex + _aloc] );
+                }
+            }
+
+            private readonly byte[] _pixelData;
+
+            private readonly int _channels;
+            private readonly int _rowStride;
+            private readonly int _rloc;
+            private readonly int _gloc;
+            private readonly int _bloc;
+            private readonly int _aloc;
+
+            public Pixels( MagickImage image )
+            {
+                _pixelData = image._mainBuffer.GetBuffer();
+
+                _channels = image.ChannelCount;
+                _rowStride = _channels * image.Width;
+                
+                var channelFormat = image._readSettings.PixelStorage.ChannelFormat;
+
+                _rloc = Math.Max( channelFormat.IndexOf( 'R' ), 0 );
+                _gloc = Math.Max( channelFormat.IndexOf( 'G' ), 0 );
+                _bloc = Math.Max( channelFormat.IndexOf( 'B' ), 0 );
+                _aloc = Math.Max( channelFormat.IndexOf( 'A' ), 0 );
+            }
+        }
+        
+        private const int MaxPooledStreams = 64;
+        private static readonly List<MemoryStream> _sStreamPool = new List<MemoryStream>();
+        
+        private static MemoryStream CreateStream()
+        {
+            MemoryStream last;
+            lock ( _sStreamPool )
+            {
+                if ( _sStreamPool.Count < 1 ) return new MemoryStream();
+                last = _sStreamPool[_sStreamPool.Count - 1];
+                _sStreamPool.RemoveAt( _sStreamPool.Count - 1 );
+            }
+            
+            last.Seek( 0, SeekOrigin.Begin );
+            last.SetLength( 0 );
+            
+            return last;
+        }
+        
+        private static void ReleaseStream( MemoryStream stream )
+        {
+            lock ( _sStreamPool )
+            {
+                if ( _sStreamPool.Count >= MaxPooledStreams ) return;
+                _sStreamPool.Add( stream );
+            }
+        }
+        
+        private MemoryStream _mainBuffer;
+        private MagickReadSettings _readSettings;
+        private Pixels _pixels;
+        
+        public int Width => _readSettings.Width;
+        public int Height => _readSettings.Height;
+        
+        public int ChannelCount => _readSettings.Format == MagickFormat.Default
+            ? _readSettings.PixelStorage.ChannelFormat.Length
+            : 4;
+        
+        private MagickImage( MagickReadSettings readSettings )
+        {
+            _mainBuffer = CreateStream();
+            _readSettings = readSettings;
+        }
+
+        public MagickImage( string filePath, MagickReadSettings readSettings )
+            : this( readSettings )
+        {
+            using ( var stream = File.Open( filePath, FileMode.Open, FileAccess.Read ) )
+            {
+                stream.CopyTo( _mainBuffer );
+            }
         }
         
         public MagickImage( Stream stream, MagickReadSettings readSettings )
+            : this( readSettings )
         {
-            throw new NotImplementedException();
+            stream.CopyTo( _mainBuffer );
         }
         
         public MagickImage( byte[] rawData, MagickReadSettings readSettings )
+            : this( readSettings )
         {
-            throw new NotImplementedException();
+            _mainBuffer.Write( rawData, 0, rawData.Length );
+        }
+
+        private bool CanReadPixels
+        {
+            get { return _readSettings.Format == MagickFormat.Default; }
+        }
+
+        private void Cli( MagickReadSettings readSettings, params string[] args )
+        {
+            var dest = CreateStream();
+
+            if ( readSettings.Width == 0 ) readSettings.Width = _readSettings.Width;
+            if ( readSettings.Height == 0 ) readSettings.Height = _readSettings.Height;
+
+            Cli( _mainBuffer, _readSettings, dest, readSettings, args );
+
+            ReleaseStream( _mainBuffer );
+            _mainBuffer = dest;
+            _readSettings = readSettings;
+
+            _pixels = null;
         }
         
-        public ImagePixels GetPixels()
+        public Pixels GetPixels()
         {
-            throw new NotImplementedException();
+            if ( !CanReadPixels )
+            {
+                Cli( new MagickReadSettings
+                {
+                    Format = MagickFormat.Default,
+                    PixelStorage = new PixelStorageSettings( StorageType.Char, "RGBA" )
+                } );
+            }
+
+            return _pixels ?? (_pixels = new Pixels( this ));
         }
         
         public void Resize( MagickGeometry geometry )
         {
-            throw new NotImplementedException();
+            var postfix = geometry.IgnoreAspectRatio ? "!" : "";
+
+            Cli( _readSettings, "-resize", $"{geometry.Width}x{geometry.Height}{postfix}" );
+
+            _readSettings.Width = geometry.Width;
+            _readSettings.Height = geometry.Height;
         }
         
         public void Write( string filePath )
         {
-            throw new NotImplementedException();
+            Cli( _mainBuffer, _readSettings, null, null, filePath );
         }
         
         public void Write( Stream stream, MagickFormat format )
         {
-            throw new NotImplementedException();
+            Cli( _mainBuffer, _readSettings, stream, new MagickReadSettings { Format = format } );
         }
         
         public void Dispose()
         {
-            
+            if ( _mainBuffer != null )
+            {
+                ReleaseStream( _mainBuffer );
+                _mainBuffer = null;
+            }
         }
     }
 }
